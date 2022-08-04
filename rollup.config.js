3e2cf145440882d453de090ffcafdc85c28e9686
weichun.swc <weichun.swc@alibaba-inc.com>
2022-08-04 13:48:11 +0800

    feat init

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import { terser } from 'rollup-plugin-terser'
+import typescript from 'rollup-plugin-typescript2'
+import size from 'rollup-plugin-size'
+
+const plugins = [
+  typescript({
+    tsconfig: 'tsconfig.json',
+    removeComments: true,
+    useTsconfigDeclarationDir: true,
+  }),
+  terser({
+    include: ['fre.js'],
+  }),
+  size()
+]
+
+module.exports = {
+  input: 'src/index.ts',
+  output: [
+    { file: 'dist/fre.umd.js', format: 'umd', name: 'fre', sourcemap: true },
+    { file: 'dist/fre.js', format: 'esm', sourcemap: true },
+    { file: 'dist/fre.esm.js', format: 'esm', sourcemap: true },
+  ],
+  plugins,
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+
+
+function hello(){
+    return 1 + 2
+}
